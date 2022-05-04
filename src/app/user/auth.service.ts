1e890d2930ab821565e8947421dcfd9ea6817831
david mimay <davidmimay@gmail.com>
2022-05-04 18:26:25 +0000

    📼 youtube subs status

---
+++ src/app/user/auth.service.ts
@@ -0,0 +1,141 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { environment } from 'src/environments/environment';
+import * as firebase from 'firebase/app';
+
+declare var gapi: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  
+  
+  user$: Observable<firebase.User>; 
+  calendarItems: any[];
+  youtubeItems: any[];
+
+
+  constructor(public afAuth: AngularFireAuth) { 
+    this.initClient();
+    this.user$ = afAuth.authState;
+  }
+
+  // Initialize the Google API client with desired scopes
+  initClient() {
+    gapi.load('client', () => {
+      console.log('loaded client')
+
+      // It's OK to expose these credentials, they are client safe.
+      gapi.client.init({
+        apiKey: environment.firebaseConfig.apiKey,
+        clientId: '920807818168-topig3d8j82hrfg47pittjlsofuqlo8b.apps.googleusercontent.com',
+        discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+        scope: 'https://www.googleapis.com/auth/calendar'
+      })
+
+      gapi.client.load('calendar', 'v3', () => console.log('loaded calendar'));
+
+    });
+
+  }
+
+  // INIT CLIENT YOUTUBE
+  /*
+  initClient() {
+    gapi.load('client', () => {
+      console.log('loaded client')
+
+      // It's OK to expose these credentials, they are client safe.
+      gapi.client.init({
+        apiKey: environment.firebaseConfig.apiKey,
+        clientId: '920807818168-topig3d8j82hrfg47pittjlsofuqlo8b.apps.googleusercontent.com',
+        discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
+        scrope: 'https://www.googleapis.com/auth/youtube.readonly'
+      })
+
+      gapi.client.load('youtube', 'v3', () => console.log('loaded youtube'));
+
+    });
+  }
+  */
+
+  async login() {
+
+    const googleAuth = gapi.auth2.getAuthInstance()
+    const googleUser = await googleAuth.signIn();
+
+    const token = googleUser.getAuthResponse().id_token;
+
+    console.log(googleUser)
+
+    const credential = auth.GoogleAuthProvider.credential(token);
+    
+    await this.afAuth.auth.signInAndRetrieveDataWithCredential(credential);
+
+
+    // Alternative approach, use the Firebase login with scopes and make RESTful API calls
+
+    // const provider = new auth.GoogleAuthProvider()
+    // provider.addScope('https://www.googleapis.com/auth/calendar');
+
+    // this.afAuth.auth.signInWithPopup(provider)
+    
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  async getYoutuber() {
+    const events = await gapi.client.youtube.subscriptions.list({
+      part: ['snippet,contentDetails'],
+      forChannelId: 'UC_x5XG1OV2P6uZZ5FSM9Ttw',
+      mine: false, //mine: true
+      })
+
+      console.log(events)
+
+      this.youtubeItems = events.result.items;
+    
+  }
+
+  async getCalendar() {
+    const events = await gapi.client.calendar.events.list({
+      calendarId: 'primary',
+      timeMin: new Date().toISOString(),
+      showDeleted: false,
+      singleEvents: true,
+      maxResults: 10,
+      orderBy: 'startTime'
+    })
+
+    console.log(events)
+
+    this.calendarItems = events.result.items;
+  
+  }
+
+  async insertEvent() {
+    const insert = await gapi.client.calendar.events.insert({
+      calendarId: 'primary',
+      start: {
+        dateTime: hoursFromNow(2),
+        timeZone: 'America/Los_Angeles'
+      }, 
+      end: {
+        dateTime: hoursFromNow(3),
+        timeZone: 'America/Los_Angeles'
+      }, 
+      summary: 'Have Fun!!!',
+      description: 'Do some cool stuff and have a fun time doing it'
+    })
+
+    await this.getCalendar();
+  }
+
+
+}
+
+const hoursFromNow = (n) => new Date(Date.now() + n * 1000 * 60 * 60 ).toISOString();
---
+++ src/app/user/youtube-subs/youtube-subs.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-youtube-subs',
+  templateUrl: './youtube-subs.component.html',
+  styleUrls: ['./youtube-subs.component.scss']
+})
+export class YoutubeSubsComponent {
+
+  constructor(private auth: AuthService) {}
+
+}
